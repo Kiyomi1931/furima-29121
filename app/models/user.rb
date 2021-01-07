eec6b15677a307cccfe6a0a4cc005793d888cbd7
Kiyomi1931 <71973734+Kiyomi1931@users.noreply.github.com>
2021-01-07 16:04:39 +0900

    モデル単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  
+  has_many:items
+  has_many:buys
+       
+  validates :nickname, presence: true
+  validates :password, presence: true, format:{with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i}
+  validates :last_name, presence: true, format:{with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name, presence: true, format:{with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :last_name_kana, presence: true, format:{with:/\A[ァ-ヶー－]+\z/}
+  validates :first_name_kana, presence: true, format:{with:/\A[ァ-ヶー－]+\z/}
+  validates :user_birth_date, presence: true
 end
