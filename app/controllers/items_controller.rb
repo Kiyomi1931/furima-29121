fd8b9db1898bbd0957d4b3e1246189b88ca49956
Kiyomi1931 <71973734+Kiyomi1931@users.noreply.github.com>
2021-01-09 12:55:56 +0900

    モデルとテーブル作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,17 @@ class ItemsController < ApplicationController
   def index
     
   end
+
+  def new
+    @item=Item.new
+  end
+
+  def create
+    Item.create
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :text).merge(user_id:current_user.id)
+  end
 end
