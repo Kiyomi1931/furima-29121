7dad7f74dd9fd2a2614143b476920bc2d5159f3b
Kiyomi1931 <71973734+Kiyomi1931@users.noreply.github.com>
2021-01-13 21:33:23 +0900

    金額入力時に販売手数料と販売利益を出力する。

---
+++ app/javascript/item_delivery_price.js
@@ -0,0 +1,12 @@
+window.addEventListener('input', () => {
+  //入力した価格の値を取得
+  const priceInput = document.getElementById("item-price");
+  const inputValue = priceInput.value;
+  //販売手数料出力
+  const addTaxDom = document.getElementById("add-tax-price");
+  addTaxDom.innerHTML = Math.floor(inputValue/10);
+  //販売利益出力
+  const servicePrice = document.getElementById("profit");
+  servicePrice.innerHTML = inputValue-Math.floor(inputValue/10);
+  
+});
